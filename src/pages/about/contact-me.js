d568597ec1e45a16989303afca6fa41ca8f26f05
Harry Wolff <hswolff@gmail.com>
2018-10-31 09:33:02 -0400

    feat: Ajaxify contact form

--- src/pages/about/contact-me.js
+++ src/pages/about/contact-me.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import _ from 'lodash';
 import Layout from '../../components/Layout';
 
 export default function ContactMePage() {
@@ -12,26 +13,80 @@ export default function ContactMePage() {
         Send me a DM on <a href="https://twitter.com/hswolff">Twitter</a> or use
         the form below.
       </p>
-      <form name="contact" method="POST" netlify>
+      <ContactForm />
+    </Layout>
+  );
+}
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
+
+class ContactForm extends React.Component {
+  state = { name: '', email: '', message: '', submitted: false };
+
+  handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': 'contact',
+        ..._.omit(this.state, ['submitted']),
+      }),
+    })
+      .then(() => this.setState({ submitted: true }))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message, submitted } = this.state;
+    if (submitted) {
+      return <span>Success. Thank you!</span>;
+    }
+    return (
+      <form onSubmit={this.handleSubmit}>
         <p>
           <label>
-            Name: <input type="text" name="name" />
+            Name:{' '}
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p>
           <label>
-            Email: <input type="email" name="email" />
+            Email:{' '}
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p>
           <label>
-            Message: <textarea name="message" />
+            <textarea
+              name="message"
+              placeholder="Message"
+              value={message}
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p>
           <button type="submit">Send</button>
         </p>
       </form>
-    </Layout>
-  );
+    );
+  }
 }
