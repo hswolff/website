32eadb97db176b2706bb5ef3052f231786f187c3
Harry Wolff <hswolff@gmail.com>
2020-05-10 14:00:58 -0400

    Switch to useStaticQuery

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 export default function SEO({
   title: titleProp,
@@ -9,57 +9,54 @@ export default function SEO({
   image: imageProp,
   isBlogPost,
 }) {
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery(graphql`
+    query LayoutData {
+      site {
+        siteMetadata {
+          title
+          description
+          siteUrl
+          keywords
+        }
+      }
+    }
+  `);
+
+  const title = titleProp || siteMetadata.title;
+  const description = descriptionProp || siteMetadata.description;
+  const image = imageProp || 'https://hswolff.com/images/profile-large.jpg';
+  let url = siteMetadata.siteUrl;
+  if (postSlug) {
+    url += postSlug;
+  }
+
   return (
-    <StaticQuery
-      query={graphql`
-        query LayoutData {
-          site {
-            siteMetadata {
-              title
-              description
-              siteUrl
-              keywords
-            }
-          }
-        }
-      `}
-      render={({ site: { siteMetadata } }) => {
-        const title = titleProp || siteMetadata.title;
-        const description = descriptionProp || siteMetadata.description;
-        const image =
-          imageProp || 'https://hswolff.com/images/profile-large.jpg';
-        let url = siteMetadata.siteUrl;
-        if (postSlug) {
-          url += postSlug;
-        }
-        return (
-          <Helmet titleTemplate={`%s | ${siteMetadata.title}`}>
-            <meta charSet="utf-8" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <meta name="keywords" content={siteMetadata.keywords} />
-            {/* The real MVP: https://moz.com/blog/meta-data-templates-123 */}
-            <title>{title}</title>
-            <meta name="description" content={description} />
-            {/* <!-- Twitter Card data --> */}
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@hswolff" />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:creator" content="@hswolff" />
-            <meta name="twitter:image" content={image} />
-            {/* <!-- Open Graph data --> */}
-            <meta property="og:title" content={title} />
-            {isBlogPost && <meta property="og:type" content="article" />}
-            <meta property="og:url" content={url} />
-            <meta property="og:image" content={image} />
-            <meta property="og:description" content={description} />
-            <meta property="og:site_name" content={siteMetadata.title} />
-          </Helmet>
-        );
-      }}
-    />
+    <Helmet titleTemplate={`%s | ${siteMetadata.title}`}>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <meta name="keywords" content={siteMetadata.keywords} />
+      {/* The real MVP: https://moz.com/blog/meta-data-templates-123 */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      {/* <!-- Twitter Card data --> */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@hswolff" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:creator" content="@hswolff" />
+      <meta name="twitter:image" content={image} />
+      {/* <!-- Open Graph data --> */}
+      <meta property="og:title" content={title} />
+      {isBlogPost && <meta property="og:type" content="article" />}
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={siteMetadata.title} />
+    </Helmet>
   );
 }
