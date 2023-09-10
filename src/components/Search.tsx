32bbbc4b1d869f66c7cd801c89466b3a1113be0e
Harry Wolff <hswolff@gmail.com>
2023-09-09 20:59:44 -0400

    Rename /posts to /blog

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -111,7 +111,7 @@ export default function SearchBar({ searchList }: Props) {
         {searchResults &&
           searchResults.map(({ item, refIndex }) => (
             <Card
-              href={`/posts/${slugify(item.data)}`}
+              href={`/blog/${slugify(item.data)}`}
               frontmatter={item.data}
               key={`${refIndex}-${slugify(item.data)}`}
             />
