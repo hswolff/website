35341db0560660c3cf8abb56bc465c45e43b4a46
Harry Wolff <hswolff@gmail.com>
2023-09-09 21:45:40 -0400

    delete unneeded files

--- __gatsby/src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from 'typography';
-import theme from 'typography-theme-parnassus';
-
-const typography = new Typography(theme);
-const { rhythm, scale } = typography;
-
-export { rhythm, scale, typography as default };
