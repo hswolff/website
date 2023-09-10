81693ad36eee58d3c2c172165b4b7ce9baa964b1
Harry Wolff <hswolff@gmail.com>
2023-09-09 21:42:07 -0400

    get rss working

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,6 +3,9 @@ import { getCollection } from 'astro:content';
 import getSortedPosts from '@utils/getSortedPosts';
 import slugify from '@utils/slugify';
 import { SITE } from '@config';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET() {
   const posts = await getCollection('blog');
@@ -11,9 +14,11 @@ export async function GET() {
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: sortedPosts.map(({ data }) => ({
-      link: `posts/${slugify(data)}`,
+    items: sortedPosts.map(({ data, body }) => ({
+      link: `blog/${slugify(data)}`,
       title: data.title,
+      category: data.category,
+      content: sanitizeHtml(parser.render(body)),
       // description: data.description,
       pubDate: new Date(data.date),
     })),
