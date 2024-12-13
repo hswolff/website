68fe0b85bd7c688c74b8a2a60e614e91f68c64f3
Harry Wolff <hswolff@gmail.com>
2024-12-12 22:36:12 -0500

    fix to newest astro collection api

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,10 @@
 import { defineCollection } from 'astro:content';
 import { blogSchema } from '@content/_schemas';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
   schema: blogSchema,
+  loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
 });
 
 export const collections = { blog };
