0ec5c78883836a842e45702522e21c58323b2ec8
Harry Wolff <hswolff@gmail.com>
2024-12-12 22:04:48 -0500

    Revert "remove unused file"
    This reverts commit c3ec65018d293baa6da03b9c938c2f14426dc119.

---
+++ src/content/_schemas.ts
@@ -0,0 +1,29 @@
+import { z } from 'astro:content';
+
+export const blogCategoryEnumValues = ['code', 'personal', 'career'] as const;
+
+export const blogSchema = z
+  .object({
+    title: z.string(),
+    // Transform string to Date object
+    date: z
+      .string()
+      .or(z.date())
+      .transform(val => new Date(val)),
+    updated: z
+      .string()
+      .or(z.date())
+      .transform(val => new Date(val))
+      .optional(),
+    category: z.enum(blogCategoryEnumValues),
+    postSlug: z.string(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().optional(),
+    featured: z.boolean().optional(),
+    ogImage: z.string().optional(),
+    description: z.string().optional(),
+    canonicalURL: z.string().optional(),
+  })
+  .strict();
+
+export type BlogFrontmatter = z.infer<typeof blogSchema>;
